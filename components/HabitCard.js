8d0dec4cb1646023932299856d3fd63b49fddf57
gpiliponyte <gretapiliponyte@Gretas-Laptop.local>
2022-12-28 19:03:47 +0200

    streak card styling

--- components/HabitCard.js
+++ components/HabitCard.js
@@ -1,7 +1,22 @@
 import styles from "../styles/Home.module.scss";
+import Image from "next/image";
 
-const HabitCard = ({h}) => {
-    return (<div className={styles.habit}><strong>{h.name}</strong><p><strong>{h.streak}</strong> days streak</p></div>);
-}
+const HabitCard = ({ h }) => {
+  return (
+    <div className={styles.habit}>
+      <strong className={styles.habitTitle}>{h.name}</strong>
+      <p>
+        <Image
+          className={styles.flame}
+          src="flame-icon.svg"
+          width={20}
+          height={20}
+          alt="streak"
+        />
+        <strong>{h.streak}</strong> days
+      </p>
+    </div>
+  );
+};
 
 export default HabitCard;
